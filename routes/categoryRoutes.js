d033aafef1ee38e2a6af7d3f58c6ddff81e4bdbb
Rejoice <uahomorejoice@gmail.com>
2023-04-11 16:34:29 +0100

    updated category routes and controller

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const addCategory = require("../categoryControllers/addCategory")
 const getAllCategory = require("../categoryControllers/getAllCategory")
+const updateCategory = require("../categoryControllers/updateCategory")
 
 const categoryRoutes = express.Router()
 
 categoryRoutes.post("/create", addCategory)
 categoryRoutes.get("/get/all", getAllCategory)
+categoryRoutes.put("/update/:id", updateCategory)
 
 module.exports = categoryRoutes
